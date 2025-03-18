8e5ce9ede41a88b6be26686fd22ec5ee63650960
Otavio Chaves <otaviocs2005@gmail.com>
2025-03-18 14:11:45 -0300

    package.json update

--- src/main.js
+++ src/main.js
@@ -10,7 +10,7 @@ const dataDisplay = document.querySelector("p");
 
 consultButton.addEventListener("click", consultCep);
 
-async function consultCep(){
+export async function consultCep(){
     const cep = cepInput.value
     try {
         const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
